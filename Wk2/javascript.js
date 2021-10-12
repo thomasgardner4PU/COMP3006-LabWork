75207120de1160203765c3390c4eeb2478fc3b50
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-10-12 15:49:07 +0100

    finished exercise 3
    provided function for Validation section

--- Wk2/javascript.js
+++ Wk2/javascript.js
@@ -1,12 +1,27 @@
 $(function() {
-    $("#sayHello").click(function() { 
+
+    $("#sayHello").click(function() {
 
         // Extract the current value of the userName input.
         let userName = $("#name").val();
-
+        console.log(userName);
         // Create the message and put it into the result paragraph
-
         let message = "Hello " + userName + "!";
-        $("sayHello_result").html(message);
+        $("#sayHello_result").html(message);
     });
+
+    console.log("works")
+    $("#numberButton").click(function () {
+
+
+        let contents = parseInt($("#theNumber").val());
+
+        if (contents < 10) {
+            $("#numberResult").html("Less than 10")
+        } else {
+            $("#numberResult").html("Greater than or equal to 10")
+        }
+    })
+
+
 })
